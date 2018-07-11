93ec2e37c02d918706e9f6c78c3dea2c57e93e2e
Jack Northover <northovers23@gmail.com>
2018-07-11 14:02:55 +1200

    first comit, bubble sort

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace sorting_algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_list = new System.Windows.Forms.Label();
+            this.btn_random_list = new System.Windows.Forms.Button();
+            this.btn_bubble_sort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_list
+            // 
+            this.lb_list.AutoSize = true;
+            this.lb_list.Location = new System.Drawing.Point(49, 65);
+            this.lb_list.Name = "lb_list";
+            this.lb_list.Size = new System.Drawing.Size(0, 20);
+            this.lb_list.TabIndex = 0;
+            // 
+            // btn_random_list
+            // 
+            this.btn_random_list.Location = new System.Drawing.Point(377, 44);
+            this.btn_random_list.Name = "btn_random_list";
+            this.btn_random_list.Size = new System.Drawing.Size(116, 62);
+            this.btn_random_list.TabIndex = 1;
+            this.btn_random_list.Text = "random list";
+            this.btn_random_list.UseVisualStyleBackColor = true;
+            this.btn_random_list.Click += new System.EventHandler(this.btn_random_list_Click);
+            // 
+            // btn_bubble_sort
+            // 
+            this.btn_bubble_sort.Location = new System.Drawing.Point(377, 161);
+            this.btn_bubble_sort.Name = "btn_bubble_sort";
+            this.btn_bubble_sort.Size = new System.Drawing.Size(116, 66);
+            this.btn_bubble_sort.TabIndex = 2;
+            this.btn_bubble_sort.Text = "bubble sort";
+            this.btn_bubble_sort.UseVisualStyleBackColor = true;
+            this.btn_bubble_sort.Click += new System.EventHandler(this.btn_bubble_sort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 520);
+            this.Controls.Add(this.btn_bubble_sort);
+            this.Controls.Add(this.btn_random_list);
+            this.Controls.Add(this.lb_list);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_list;
+        private System.Windows.Forms.Button btn_random_list;
+        private System.Windows.Forms.Button btn_bubble_sort;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sorting_algorithm
+{
+    public partial class Form1 : Form
+    {
+        Random R_ran = new Random();
+        int number = 0;
+        int[] ai_sorting_list;
+        int list_count = 10;
+        string display_list = null;
+        int check1 = 0;
+        int check2 = 0;
+        int total_sorted = 0;
+        int total_comparosins = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_random_list_Click(object sender, EventArgs e)
+        {
+            ai_sorting_list = new int[list_count];
+            for (int count = 1; count <= list_count; count ++)
+            {
+                startpoint:
+                number = R_ran.Next(0, list_count);
+                if (ai_sorting_list[number] == 0)
+                {
+                    ai_sorting_list[number] = count;
+                }
+                else { goto startpoint; }
+            }
+            display();
+        }
+        private void display()
+        {
+            display_list = null;
+            for (int count = 1; count <= list_count; count++)
+            {
+                display_list += Convert.ToString(ai_sorting_list[count - 1]) + Environment.NewLine;
+            }
+            lb_list.Text = display_list;
+
+        }
+
+        private void btn_bubble_sort_Click(object sender, EventArgs e)
+        {
+            check1 = 0;
+            check2 = 0;
+            total_sorted = 0;
+            total_comparosins = 0;
+            Start:
+            for (int comparisons = 0; comparisons <= list_count - total_sorted - 2; comparisons++)
+            {
+                check1 = ai_sorting_list[comparisons];
+                check2 = ai_sorting_list[comparisons + 1];
+
+                if (check1 > check2)
+                {
+                    ai_sorting_list[comparisons] = check2;
+                    ai_sorting_list[comparisons + 1] = check1;
+                }
+                total_comparosins += 1;
+            }
+            total_sorted += 1;
+            if (total_sorted != 9) { goto Start; }
+            else { display(); }
+        }
+    }
+}
